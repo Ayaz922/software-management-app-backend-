51f275bb93995629f1d393d9c269e1bda7a2cf3c
Ayaz Alam <31290706+ayaz-alam@users.noreply.github.com>
2021-05-02 19:42:15 +0530

    Delete dist directory

--- dist/utils/testfile.js
+++
@@ -1,8 +0,0 @@
-const e = require('express');
-const enums = require('./enums');
-input = 'BACKLOGd';
-if (enums.TASK_STATUS.hasOwnProperty(input))
-    console.log('Valid input');
-else
-    console.log('Invalid Input');
-console.log(enums.TASK_STATUS.BACKLOG);
